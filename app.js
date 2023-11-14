ec33190e5a8c0ad6a929500a31e9743e5fcc1012
Komaramhemanth <140368896+Komaramhemanth@users.noreply.github.com>
2023-11-14 13:31:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    // In a real application, you would perform authentication here.
+    // For simplicity, we're just checking if both fields are filled.
+    if (username && password) {
+      setLoggedIn(true);
+    }
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
+
+  return (
+    <div className="App">
+      {loggedIn ? (
+        <div>
+          <h1>Welcome, {username}!</h1>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <h1>Login</h1>
+          <form>
+            <label>
+              Username:
+              <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+            <br />
+            <label>
+              Password:
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </label>
+            <br />
+            <button type="button" onClick={handleLogin}>
+              Login
+            </button>
+          </form>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
