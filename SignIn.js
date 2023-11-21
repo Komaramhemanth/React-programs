7cdfd5ebde68811cfd2340ff6d4d036b05f69911
Komaramhemanth <140368896+Komaramhemanth@users.noreply.github.com>
2023-11-21 06:23:01 +0530

    Add files via upload

---
+++ SignIn.js
@@ -0,0 +1,37 @@
+// src/SignIn.js
+import React, { useState } from 'react';
+
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your sign-in logic here
+    console.log('Email:', email, 'Password:', password);
+  };
+
+  return (
+    <div>
+      <h2>Sign In</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={handleEmailChange} required />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} required />
+        </label>
+        <br />
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
