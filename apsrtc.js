e3409fa8615302b49f72e141daa5a8a7451175a1
Komaramhemanth <140368896+Komaramhemanth@users.noreply.github.com>
2023-11-30 12:04:26 +0530

    Add files via upload

---
+++ apsrtc.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+  
+<script>
+class Car {
+  constructor(name) {
+    this.brand = name;
+  }
+}
+class bus {
+constructor(name){
+this.brand=name;
+}
+}
+const mycar = new Car("FordInova");
+
+const mybus= new bus("APSRTC");
+
+document.write(mycar.brand);
+document.write(mybus.brand);
+</script>
+
+</body>
+</html>
