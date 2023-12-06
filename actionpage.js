aca70d9e67237b544e453eae69b035413707df2f
Komaramhemanth <140368896+Komaramhemanth@users.noreply.github.com>
2023-12-06 20:14:34 +0530

    Add files via upload

---
+++ actionpage.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>How To Create an Email Field</h2>
+
+<p>Show an email field:</p>
+<form action="/action_page.php">
+  <label for="email">Enter your email:</label>
+  <input type="email" id="email" name="email">
+  <input type="submit">
+</form>
+
+</body>
+</html>
